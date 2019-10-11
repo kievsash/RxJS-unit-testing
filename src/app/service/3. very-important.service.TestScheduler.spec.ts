06580839ac2b83790ed507b60a1c4ff49742e446
Oleksandr Poshtaruk <kievsash@gmail.com>
2019-10-11 18:59:21 +0300

    TestScheduler fix

--- src/app/service/3. very-important.service.TestScheduler.spec.ts
+++ src/app/service/3. very-important.service.TestScheduler.spec.ts
@@ -1,129 +1,125 @@
-// import {TestBed} from '@angular/core/testing';
-//
-// import {asapScheduler, asyncScheduler, from, of, VirtualTimeScheduler} from 'rxjs';
-// import {concatMap, delay, switchMap} from 'rxjs/operators';
-// import {VeryImportantServiceTS} from './3. very-important.service.TestScheduler';
-// import {TestScheduler} from 'rxjs/testing';
-//
-// describe('VeryImportantServiceTS', () => {
-//   let service;
-//   let mockHttp;
-//
-//   beforeEach(() => {
-//     mockHttp = {get: () => of(42, asyncScheduler)};
-//
-//     service = new VeryImportantServiceTS(mockHttp);
-//   });
-//
-//   describe('getData (use TestScheduler as VirtualTimeScheduler)', () => {
-//     it('should emit 3 specific values', () => {
-//       const assertion = (actual, expected) => {
-//         expect(actual).toEqual(expected);
-//       };
-//       const scheduler = new TestScheduler(assertion);
-//       scheduler.maxFrames = Number.POSITIVE_INFINITY;
-//
-//       service.http = {get: () => of(42, scheduler)};
-//
-//       const range$ = service.getData(30, scheduler);
-//       const result = [];
-//
-//       range$.subscribe({
-//         next: (value) => {
-//           result.push(value);
-//         }
-//       });
-//
-//       scheduler.flush();
-//       expect(result).toEqual([42, 42, 42]);
-//     });
-//   });
-//
-//   describe('getData (TestScheduler with marbles)', () => {
-//     it('should emit 3 values', () => {
-//       const assertion = (actual, expected) => {
-//         expect(actual).toEqual(expected);
-//       };
-//       const scheduler = new TestScheduler(assertion);
-//       (asyncScheduler.constructor as any).delegate = scheduler;
-//
-//       const marbleValues = {a: 42};
-//       service.http = {get: () => scheduler.createColdObservable('(a|)', marbleValues)};
-//
-//       const expectedMarble = 'a-a-(a|)';
-//
-//       scheduler.expectObservable(service.getData(0.02)).toBe(expectedMarble, marbleValues);
-//
-//       scheduler.flush();
-//
-//       (asyncScheduler.constructor as any).delegate = undefined;
-//     });
-//   });
-//
-//   describe('getRangeASAP (with trick)', () => {
-//     it('should emit 4 specific values (with trick)', () => {
-//       const assertion = (actual, expected) => {
-//         expect(actual).toEqual(expected);
-//       };
-//       const scheduler = new TestScheduler(assertion);
-//       asapScheduler.schedule = scheduler.schedule.bind(
-//         scheduler
-//       ) as any;
-//
-//       const range$ = service.getRangeASAP();
-//       const result = [];
-//       range$.subscribe({
-//         next: (value) => {
-//           result.push(value);
-//         }
-//       });
-//
-//       scheduler.flush();
-//       expect(result).toEqual([0, 1, 2, 3]);
-//
-//       delete asapScheduler.schedule;
-//     });
-//   });
-//
-//   describe('getRangeASAP (with trick and marbles)', () => {
-//     it('should emit 4 specific values (with trick)', () => {
-//       const assertion = (actual, expected) => {
-//         expect(actual).toEqual(expected);
-//       };
-//       const scheduler = new TestScheduler(assertion);
-//       asapScheduler.schedule = scheduler.schedule.bind(
-//         scheduler
-//       ) as any;
-//
-//       const marbleValues = {a: 0, b: 1, c: 2, d: 3};
-//       const expectedMarble = '(abcd|)';
-//
-//       scheduler.expectObservable(service.getRangeASAP()).toBe(expectedMarble, marbleValues);
-//
-//       scheduler.flush();
-//
-//       delete asapScheduler.schedule;
-//     });
-//   });
-//
-//   describe('watchTwoEmissions', () => {
-//     it('should merge values emissions', () => {
-//       const assertion = (actual, expected) => {
-//         expect(actual).toEqual(expected);
-//       };
-//       const scheduler = new TestScheduler(assertion);
-//       (asyncScheduler.constructor as any).delegate = scheduler;
-//
-//       const marbleValues = {a: 42, b: 13};
-//       service.searchStringChange$ = scheduler.createColdObservable('--a--|', marbleValues);
-//       service.paginationChange$ =   scheduler.createColdObservable('b--|', marbleValues);
-//
-//       const expectedMarble = 'b-a--|';
-//
-//       scheduler.expectObservable(service.watchTwoEmissions()).toBe(expectedMarble, marbleValues);
-//
-//       (asyncScheduler.constructor as any).delegate = undefined;
-//     });
-//   });
-//
-// });
+import {TestBed} from '@angular/core/testing';
+
+import {asapScheduler, asyncScheduler, from, of, VirtualTimeScheduler} from 'rxjs';
+import {concatMap, delay, switchMap} from 'rxjs/operators';
+import {VeryImportantServiceTS} from './3. very-important.service.TestScheduler';
+import {TestScheduler} from 'rxjs/testing';
+
+describe('VeryImportantServiceTS', () => {
+  let service;
+  let mockHttp;
+
+  beforeEach(() => {
+    mockHttp = {get: () => of(42, asyncScheduler)};
+
+    service = new VeryImportantServiceTS(mockHttp);
+  });
+
+  describe('getData (use TestScheduler as VirtualTimeScheduler)', () => {
+    it('should emit 3 specific values', () => {
+      const assertion = (actual, expected) => {
+        expect(actual).toEqual(expected);
+      };
+      const scheduler = new TestScheduler(assertion);
+      scheduler.maxFrames = Number.POSITIVE_INFINITY;
+
+      service.http = {get: () => of(42, scheduler)};
+
+      const range$ = service.getData(30, scheduler);
+      const result = [];
+
+      range$.subscribe({
+        next: (value) => {
+          result.push(value);
+        }
+      });
+
+      scheduler.flush();
+      expect(result).toEqual([42, 42, 42]);
+    });
+  });
+
+  describe('getData (TestScheduler with marbles)', () => {
+    it('should emit 3 values', () => {
+      const assertion = (actual, expected) => {
+        expect(actual).toEqual(expected);
+      };
+      const scheduler = new TestScheduler(assertion);
+      (asyncScheduler.constructor as any).delegate = scheduler;
+
+      const marbleValues = {a: 42};
+      service.http = {get: () => scheduler.createColdObservable('(a|)', marbleValues)};
+
+      const expectedMarble = 'a-a-(a|)';
+
+      scheduler.expectObservable(service.getData(0.02)).toBe(expectedMarble, marbleValues);
+
+      scheduler.flush();
+
+      (asyncScheduler.constructor as any).delegate = undefined;
+    });
+  });
+
+  describe('getRangeASAP (with trick)', () => {
+    it('should emit 4 specific values (with trick)', () => {
+      const assertion = (actual, expected) => {
+        expect(actual).toEqual(expected);
+      };
+      const scheduler = new TestScheduler(assertion);
+      (asyncScheduler as any).constructor.delegate = scheduler;
+
+      const range$ = service.getRangeASAP();
+      const result = [];
+      range$.subscribe({
+        next: (value) => {
+          result.push(value);
+        }
+      });
+
+      scheduler.flush();
+      expect(result).toEqual([0, 1, 2, 3]);
+
+      (asyncScheduler as any).constructor.delegate = undefined;
+    });
+  });
+
+  describe('getRangeASAP (with trick and marbles)', () => {
+    it('should emit 4 specific values (with trick)', () => {
+      const assertion = (actual, expected) => {
+        expect(actual).toEqual(expected);
+      };
+      const scheduler = new TestScheduler(assertion);
+      (asyncScheduler as any).constructor.delegate = scheduler;
+
+      const marbleValues = {a: 0, b: 1, c: 2, d: 3};
+      const expectedMarble = '(abcd|)';
+
+      scheduler.expectObservable(service.getRangeASAP()).toBe(expectedMarble, marbleValues);
+
+      scheduler.flush();
+
+      (asyncScheduler as any).constructor.delegate = undefined;
+    });
+  });
+
+  describe('watchTwoEmissions', () => {
+    it('should merge values emissions', () => {
+      const assertion = (actual, expected) => {
+        expect(actual).toEqual(expected);
+      };
+      const scheduler = new TestScheduler(assertion);
+      (asyncScheduler.constructor as any).delegate = scheduler;
+
+      const marbleValues = {a: 42, b: 13};
+      service.searchStringChange$ = scheduler.createColdObservable('--a--|', marbleValues);
+      service.paginationChange$ = scheduler.createColdObservable('b--|', marbleValues);
+
+      const expectedMarble = 'b-a--|';
+
+      scheduler.expectObservable(service.watchTwoEmissions()).toBe(expectedMarble, marbleValues);
+
+      (asyncScheduler.constructor as any).delegate = undefined;
+    });
+  });
+
+});
